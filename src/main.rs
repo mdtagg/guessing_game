2639d40d6e3e1aec53c47535a292c4102af42738
Michael Tagg <mdtagg91@gmail.com>
2026-07-27 14:10:35 -0700

    commenting add

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ fn main() {
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin()
+        //&mut is used to first define the guess string as being mutable
+        //its used again to say "let .readline change guess now but only now, main still retains
+        //ownership"
         .read_line(&mut guess)
+        //.expect -> value, Result = enum, a type that can be in one of
+        //multiple different states
         .expect("Failed to read line");
     println!("You guessed:{guess}");
 }
