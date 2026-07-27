619279a5f6f9eb042eb91d07189208b0937bfea0
Michael Tagg <mdtagg91@gmail.com>
2026-07-27 16:08:51 -0700

    less verbose comments add

--- src/main.rs
+++ src/main.rs
@@ -10,21 +10,19 @@ fn main() {
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin()
-        //&mut is used to first define the guess string as being mutable
-        //its used again to say "let .readline change guess now but only now, main still retains
-        //ownership"
+        //&mut defines guess as mutable
+        //2nd time "let .readline change guess only now, main retains ownership"
         .read_line(&mut guess)
-        //.expect -> value, Result = enum, a type that can be in one of
-        //multiple different states
+        //.expect -> value, Result = enum, a type in one of multiple states
         .expect("Failed to read line");
-    //Rust allows "shadowing" creating a variable with the same name of different type
-    //prevents having to create variables with new names only to change a type
+    //Rust "shadowing" -> creating a variable with the same name of different type
+    //prevents variables with new names only to change a type
     //The : u32 tells rust which type to convert to in the parse function
     let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("You guessed:{guess}");
 
-    //match is an expression made up of "arms" similar to switch cases
-    //here it compares guess to secret_number, can be used on anything comparable
+    //match is an expression made up of "arms",similar to switch cases
+    //compares guess to secret_number, can be used on anything comparable
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
